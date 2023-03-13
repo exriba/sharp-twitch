b9ba81e9e4000999149aa77ca9ed2ffd8cc2882b
exriba <44509247+exriba@users.noreply.github.com>
2023-03-13 18:45:48 -0400

    updated test cases

--- SharpTwitch.Auth.Tests/Helpers/AuthUtilsTests.cs
+++ SharpTwitch.Auth.Tests/Helpers/AuthUtilsTests.cs
@@ -6,21 +6,24 @@ namespace SharpTwitch.Auth.Tests.Helpers
 {
     public class AuthUtilsTests
     {
-        private const string ClientId = "clientId";
-        private const string Uri = "http://localhost:1234";
+        private const string EMPTY_STRING = "";
+        private const string WHITESPACE_STRING = " ";
+        private const string CLIENT_ID = "clientId";
+        private const string URI = "http://localhost:1234";
 
         [Theory]
-        [InlineData("", null, null)]
-        [InlineData(" ", null, null)]
-        [InlineData(null, null, null)]
-        [InlineData(ClientId, "", null)]
-        [InlineData(ClientId, " ", null)]
-        [InlineData(ClientId, null, null)]
-        [InlineData(ClientId, Uri, null)]
+        [InlineData(null, URI, null)]
+        [InlineData(EMPTY_STRING, URI, null)]
+        [InlineData(WHITESPACE_STRING, URI, null)]
+        [InlineData(CLIENT_ID, null, null)]
+        [InlineData(CLIENT_ID, EMPTY_STRING, null)]
+        [InlineData(CLIENT_ID, WHITESPACE_STRING, null)]
+        [InlineData(CLIENT_ID, URI, null)]
         public void GenerateAuthorizationUrl_ShouldHandleInvalidArgs(string clientId, string redirectUri, IEnumerable<Scope> scopes)
         {
             var forceVerify = false;
             var state = Guid.NewGuid().ToString();
+            
             Assert.ThrowsAny<ArgumentException>(() => AuthUtils.GenerateAuthorizationUrl(clientId, redirectUri, scopes, state, forceVerify));
         }
 
@@ -30,9 +33,10 @@ namespace SharpTwitch.Auth.Tests.Helpers
             var state = Guid.NewGuid().ToString();
             var scopes = new Scope[] { Scope.ANALYTICS_READ_EXTENSIONS }.ToList();
 
-            var authUrl = AuthUtils.GenerateAuthorizationUrl(ClientId, Uri, scopes, state);
-            Assert.Contains($"client_id={ClientId}", authUrl);
-            Assert.Contains($"redirect_uri={HttpUtility.UrlEncode(Uri)}", authUrl);
+            var authUrl = AuthUtils.GenerateAuthorizationUrl(CLIENT_ID, URI, scopes, state);
+
+            Assert.Contains($"client_id={CLIENT_ID}", authUrl);
+            Assert.Contains($"redirect_uri={HttpUtility.UrlEncode(URI)}", authUrl);
             Assert.Contains("scope=analytics:read:extensions", authUrl);
             Assert.Contains($"state={state}", authUrl);
         }
