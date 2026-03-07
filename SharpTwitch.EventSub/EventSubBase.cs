df4aa11e8ddfcf854975804a97c0583cfd86bfb3
exriba <44509247+exriba@users.noreply.github.com>
2026-03-06 21:05:24 -0500

    Update EventSubBase.cs

--- SharpTwitch.EventSub/EventSubBase.cs
+++ SharpTwitch.EventSub/EventSubBase.cs
@@ -12,13 +12,13 @@ namespace SharpTwitch.EventSub
         /// </summary>
         /// <param name="subscriptionType">subscription type</param>
         /// <param name="args">event arguments</param>
-        public abstract void RaiseEvent(SubscriptionType subscriptionType, EventArgs args);
+        internal abstract void RaiseEvent(SubscriptionType subscriptionType, EventArgs args);
 
         /// <summary>
         /// Invokes exception event handler for the given subscription type. 
         /// </summary>
         /// <param name="subscriptionType">subscription type</param>
         /// <param name="exception">exception</param>
-        public abstract void RaiseErrorEvent(SubscriptionType subscriptionType, Exception exception);
+        internal abstract void RaiseErrorEvent(SubscriptionType subscriptionType, Exception exception);
     }
 }
